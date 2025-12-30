64248a1392c9c25e2312819e30c153508962e23d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-12-30 15:36:27 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# LinkShell
+
+LinkStationの簡易的なシェル接続ツール。
+LinkStationのssh環境をセットアップする用途で使用する想定です。
+（初代ポケモンでのバイナリエディタを作るときに使うプログラムマシンのようなイメージ）
+
+> [!WARNING]
+> 本ツールの使用により発生したいかなる損害についても、作者は一切の責任を負いません。自己責任でご利用ください。
+
+## 動作環境
+
+- Ruby 3.4
+- Java Runtime
+- LS-WVL/R1 本体
+- [Buffalo LinkStation Series Updater Ver1.75](https://www.buffalo.jp/support/download/detail/?dl_contents_id=60849)
+
+## 使い方
+
+### 元に戻す方法
+
+1. Updaterのlinkstation_version.ini、linkstation_version.txtのバージョン情報を書き換える
+
+この２つのファイルの`2025/12/08`という日付を未来に進め、`VERSION`を`1.75-0.02`のように変えてあげます。
+
+2. Updaterを実行する
+
+なお、以下のようなバージョンが変わっていない旨の警告が出ますが、特に問題はないです：
+
+```
+LinkStationのファームウェアが書き換わったことを確認できませんでした
+kernel [2020/12/08 15:03:07]  （LinkStationのファームウェア）
+kernel [2020/12/09 15:03:07]  （アップデート後の正しいファームウェア）
+
+ファームウェアのアップデートに失敗した可能性があります
+```
+
+### LinkShellの使い方
+
+```
+❯ ruby linkshell.rb -h
+Usage: linkshell.rb [options]
+    -t, --target TARGET              Your LinkStation's IP address
+    -p, --password PASSWORD          Your LinkStation's admin password
+    -i, --interval SECONDS           Interval between commands
+    -T, --tries N                    Number of tries for each command
+    -v, --verbose                    Run verbosely
+    -h, --help                       Show this help message
+```
+
+## 仕組み
+
+LS-WVL/R1にはbusyboxが入っています。
+busyboxで動く簡易的な遠隔シェルをacp-commander経由でインストールし、それに接続しています。
+
+## ライセンス
+
+MIT Licenseで公開しています。
---
+++ linkshell.rb
@@ -0,0 +1,125 @@
+# Buffalo LinkStation SSH Enabler
+require "optparse"
+require "shellwords"
+require "io/console"
+require "open3"
+require "digest"
+
+options = {}
+
+parse =
+  OptionParser.new do |opts|
+    opts.banner = "Usage: linkshell.rb [options]"
+
+    opts.on("-t", "--target TARGET", "Your LinkStation's IP address") do |v|
+      options[:target] = v
+    end
+    opts.on(
+      "-p",
+      "--password PASSWORD",
+      "Your LinkStation's admin password"
+    ) { |v| options[:password] = v }
+    opts.on("-i", "--interval SECONDS", "Interval between commands") do |v|
+      options[:interval] = v.to_f
+    end
+    opts.on(
+      "-T",
+      "--tries N",
+      Integer,
+      "Number of tries for each command"
+    ) { |v| options[:tries] = v }
+    opts.on("-v", "--verbose", "Run verbosely") { |v| options[:verbose] = v }
+    opts.on("-h", "--help", "Show this help message") do
+      puts opts
+      exit
+    end
+  end
+
+parse.parse!
+unless options[:target]
+  raise OptionParser::MissingArgument, "Target IP address is required"
+end
+
+unless options[:password]
+  print "Enter admin password of your LinkStation: "
+  options[:password] = STDIN.noecho(&:gets).chomp
+  puts
+end
+
+commands = []
+
+shell_port = 11511
+payload = <<~PAYLOAD
+  PORT=#{shell_port}
+
+  if [ -f /tmp/nc_shell_pid ]; then
+    OLD_PID=$(cat /tmp/nc_shell_pid)
+    kill $OLD_PID 2>/dev/null
+  fi
+
+  echo "$$" > /tmp/nc_shell_pid
+
+  busybox nc -lp $PORT -e /bin/sh -c 'echo Successfully connected to LinkShell!; PS1="\\u@\\h:\\w\\$ " ; export PS1; exec /bin/sh -i'
+
+  rm -f /tmp/nc_shell_pid
+PAYLOAD
+
+shellname = "/tmp/linkshell_#{Digest::MD5.hexdigest(payload)}.sh"
+puts "Generated shell script will be installed at #{shellname}"
+
+commands << "rm -f #{shellname}.tmp"
+commands << "rm -f /tmp/linkshell_*"
+payload.lines.each do |line|
+  line
+    .chars
+    .each_slice(90) do |chunk|
+      if chunk[-1] == "\n"
+        commands << "echo #{chunk.join.shellescape} >> #{shellname}.tmp"
+      else
+        commands << "echo -n #{chunk.join.shellescape} >> #{shellname}.tmp"
+      end
+    end
+end
+commands << "chmod +x #{shellname}.tmp"
+commands << "mv #{shellname}.tmp #{shellname}"
+commands << "nohup sh #{shellname} >/dev/null 2>&1 &"
+
+# as interactive mode does not run more than one command, we spam the `-c` option
+real_commands =
+  commands.map do |command|
+    raise "Command too long: #{command}" if command.size >= 200
+    "java -jar ./external/acp-commander/acp_commander.jar -t #{options[:target]} -ip #{options[:target]} -pw #{options[:password]} -c #{command.shellescape}"
+  end
+is_installed =
+  `java -jar ./external/acp-commander/acp_commander.jar -t #{options[:target]} -ip #{options[:target]} -pw #{options[:password]} -c "echo -n __shellcheck:; if [ -f #{shellname} ]; then echo exists; else echo missing; fi"`.chomp
+if is_installed.include?("__shellcheck:exists")
+  puts "Remote shell already installed."
+elsif is_installed.include?("__shellcheck:missing")
+  puts "Installing remote shell..."
+  real_commands.each do |command|
+    remaining_tries = options[:tries] || 3
+    remaining_tries.times do |attempt|
+      puts "Executing: #{command} (Attempt #{attempt + 1}/#{remaining_tries})"
+      if options[:verbose]
+        system command
+      else
+        system command, out: File::NULL, err: File::NULL
+      end
+      if $?.exitstatus == 0
+        break
+      else
+        puts "Command failed."
+      end
+      if attempt + 1 == remaining_tries
+        raise "Command failed after #{remaining_tries} attempts: #{command}"
+      end
+    end
+    sleep options[:interval] if options[:interval]
+  end
+else
+  puts is_installed
+  raise "Failed to check if remote shell is installed"
+end
+puts "Starting remote shell..."
+sleep 2
+system "nc #{options[:target]} #{shell_port}"
